77363ddc6c4916299b91a16d2577f5fa80a55b9c
liliaungurean <lilicikau95@gmail.com>
2023-02-02 18:29:44 -0600

    models

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
+const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
 let requelize;
 
@@ -10,7 +11,7 @@ if (process.env.JAWSDB_URL){
   new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3003
+    port: 3005
   });
 }
 module.exports = sequelize;
