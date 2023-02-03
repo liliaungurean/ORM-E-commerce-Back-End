a7b95679973b8bdeab1fb740edfbb72678d9413e
liliaungurean <lilicikau95@gmail.com>
2023-02-02 18:01:27 -0600

    final commit

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let requelize;
 
+if (process.env.JAWSDB_URL){
+  sequelize = new Sequelize (process.env.JAWSDB_URL);
+} else {
+  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3003
+  });
+}
 module.exports = sequelize;
